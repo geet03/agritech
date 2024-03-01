54f82aaa25b453ef863d0cb01b724ed7737d5fa4
Geetanjali Khabale <gitanjalikhabale@gmail.com>
2024-03-01 11:56:42 +0000

    Initial commit

---
+++ patilgroup/src/main/java/com/agritech/patilgroup/PatilgroupApplication.java
@@ -0,0 +1,13 @@
+package com.agritech.patilgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatilgroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatilgroupApplication.class, args);
+	}
+
+}
---
+++ patilgroup/src/test/java/com/agritech/patilgroup/PatilgroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agritech.patilgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatilgroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
